c5418f3f1aa73e5339b0792e58aae1684252124d
nachoNota <nachonota13@gmail.com>
2024-04-09 22:49:14 -0300

    punto 4 terminado

--- tp2_4.c
+++ tp2_4.c
@@ -11,32 +11,89 @@ struct {
 
 char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
-void mostrar_compus(compu *punt, char *punt_aux);
+void mostrar_compus(compu *punt);
 void mas_vieja(compu *punt);
 void mas_nueva(compu *punt);
-
+void mas_rapida(compu *punt); 
 
 int main(){
-    compu clientes[5], *punt;
-    char *punt_aux = tipos;
+    compu compus[5], *punt;
 
-    punt = clientes;
-    mostrar_compus(punt, punt_aux);
+    punt = compus;
+    mostrar_compus(punt);
+    mas_vieja(punt);
+    mas_nueva(punt);
+    mas_rapida(punt);
+
+    for (int i = 0; i < 5; i++)
+    {
+        free(punt);
+        free(compus[i].tipo_cpu);
+    }
     return 0;
 }
 
-void mostrar_compus(compu *punt, char *punt_aux){
-    int random;
+void mostrar_compus(compu *punt){
     for (int i = 0; i < 5; i++)
     {
         punt->velocidad = rand() % 3 + 1;
         punt->anio = rand() % 9 + 2015;
         punt->cantidad = rand() % 9 + 1;
-        random = rand() % 6 + 1;
-        punt->tipo_cpu =  
+        punt->tipo_cpu =  tipos[rand() % 6];
+        punt++;
     }
-    
-    
+
+    punt = punt - 5;
+
+    for (int i = 0; i < 5; i++){
+        printf("------COMPU %d-----\n", i+1);
+        printf("Velocidad: %d GHz\n", punt->velocidad);
+        printf("Anio de fabricacion: %d\n", punt->anio);
+        printf("Cantidad de nucleos: %d\n", punt->cantidad);
+        printf("Tipo de procesador: %s\n", punt->tipo_cpu);
+        punt++;
+    }
+        
 }
-void mas_vieja(compu *punt);
-void mas_nueva(compu *punt);
+
+void mas_vieja(compu *punt){
+    int men = 2023; // Inicializar con un valor menor al rango de años posibles
+    int pos = 0;
+    for (int i = 0; i < 5; i++) {
+        if (punt->anio < men) {
+            men = punt->anio;
+            pos = i + 1; // Incrementar la posición en 1 para obtener la posición correcta
+        }
+        punt++;
+    }
+
+    printf("La compu mas vieja es %d, con %d anios\n", pos, men);
+}
+
+void mas_nueva(compu *punt){
+    int may = 2015; // Inicializar con un valor menor al rango de años posibles
+    int pos = 0;
+    for (int i = 0; i < 5; i++) {
+        if (punt->anio > may) {
+            may = punt->anio;
+            pos = i + 1; // Incrementar la posición en 1 para obtener la posición correcta
+        }
+        punt++;
+    }
+
+    printf("La compu mas vieja es %d, con %d anios\n", pos, may);
+}
+
+void mas_rapida(compu *punt){
+    int may = 1; // Inicializar con un valor menor al rango de años posibles
+    int pos = 0;
+    for (int i = 0; i < 5; i++) {
+        if (punt->velocidad > may) {
+            may = punt->velocidad;
+            pos = i + 1; // Incrementar la posición en 1 para obtener la posición correcta
+        }
+        punt++;
+    }
+
+    printf("La compu mas rapida es %d, con %d GHz\n", pos, may);
+}
